94f27cb1c3fa98e8082fe6b39f7f0788063f5eec
kztmk <kazuya@tmksolutions.com>
2023-02-22 16:26:29 -0800

    fix .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,39 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'plugin:storybook/recommended',
+  ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint', 'prettier', 'testing-library', 'jest-dom'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/function-component-definition': [2, {
-      namedComponents: 'arrow-function' | 'function-declaration'
-    }],
-    'no-param-reassign': ['error', {
-      props: false
-    }]
-  }
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
+  },
 };
