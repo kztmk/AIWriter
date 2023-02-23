bea1485aa96d186c750872113fbe95f2226de2b2
kztmk <kazuya@tmksolutions.com>
2023-02-23 11:02:47 -0800

    fix firebase file

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,5 @@
 import { getAnalytics } from 'firebase/analytics';
-import { getDatabase } from 'firebase/database';
-import app from '../main';
 
-export const analytics = getAnalytics(app);
-export const db = getDatabase(app);
+const analytics = getAnalytics();
+
+export default analytics;
